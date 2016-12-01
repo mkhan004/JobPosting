d6fbc2bfaacf63bfc77304b1f7bdf732bafc9036
Shakil Khan <shakil.khan@kaplan.com>
2016-11-30 21:25:34 -0500

    add project structure

---
+++ src/main/java/com/JobPosting/JobPosting/App.java
@@ -0,0 +1,13 @@
+package com.JobPosting.JobPosting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
